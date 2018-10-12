d489a2add8b2907db996fc7cf558c017da4ee078
AlexisChen <chenming@usc.edu>
2018-10-11 17:41:11 -0700

    organize project

---
+++ renshenglogin/renshenglogin/ViewController/MessageViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MessageViewController.swift
+//  renshenglogin
+//
+//  Created by Alexis Chen on 3/20/18.
+//  Copyright © 2018 Alexis Chen. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
